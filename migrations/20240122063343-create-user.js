fb3c27ad221a22ba65f44ebd8162388bf244e71d
N.v.v.s.sudheer <110334202+narkidimillisudheer@users.noreply.github.com>
2024-02-10 18:04:01 +0530

    adding migrations

--- migrations/20240122063343-create-user.js
+++ migrations/20240122063343-create-user.js
@@ -34,6 +34,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("Users");
+    await queryInterface.dropTable("Users"); //storing the data in the users table
   },
 };
